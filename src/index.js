7a02248b584c0e7b5fd6fac44b23cb350c5a8cb2
christhian12rv <christhian11rv@hotmail.com>
2022-03-09 16:49:28 -0300

    Make changes to heroku

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ app.use(
     })
 );
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 sequelize
     .sync()
