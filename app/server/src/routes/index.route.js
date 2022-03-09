576b938101e04f93b79b2c02ea4f9fa04921d8d1
christhian12rv <christhian11rv@hotmail.com>
2022-03-09 15:27:38 -0300

    Make changes to heroku

--- app/server/src/routes/index.route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const indexController = require("../controllers/index.controller");
-
-router.get("/total", indexController.getTotal);
-
-module.exports = router;
